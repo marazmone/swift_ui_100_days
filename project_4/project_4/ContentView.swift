3b8d51a4f7a7a739e08b4b14a8b7bdf8293c52c9
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-07-15 14:40:36 +0200

    Update UI
    - remove alert dialog
    - add picker for select cups

--- project_4/project_4/ContentView.swift
+++ project_4/project_4/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 3
     
     @State private var alertTitle = ""
     @State private var alertMessage = ""
@@ -26,24 +26,46 @@ struct ContentView: View {
     }
     
     var body: some View {
+        let bindingWake = Binding(
+            get: { wakeUp },
+            set: {
+                wakeUp = $0
+                calculateBedtime()
+            }
+        )
+        let bindingSleepAmount = Binding(
+            get: { sleepAmount },
+            set: {
+                sleepAmount = $0
+                calculateBedtime()
+            }
+        )
+        let bindingCoffeeAmount = Binding(
+            get: { coffeeAmount },
+            set: {
+                coffeeAmount = $0
+                calculateBedtime()
+            }
+        )
+        
         NavigationView {
             Form {
                 Section {
                     VStack(alignment: .leading, spacing: 10) {
                         Text("When do you want to wake up?")
                             .font(.headline)
-
-                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        
+                        DatePicker("Please enter a time", selection: bindingWake, displayedComponents: .hourAndMinute)
                             .labelsHidden()
                     }
                 }
-
+                
                 Section {
                     VStack(alignment: .leading, spacing: 10) {
                         Text("Desired amount of sleep")
                             .font(.headline)
-
-                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                        
+                        Stepper("\(sleepAmount.formatted()) hours", value: bindingSleepAmount, in: 4...12, step: 0.25)
                     }
                 }
                 
@@ -51,20 +73,32 @@ struct ContentView: View {
                     VStack(alignment: .leading, spacing: 10) {
                         Text("Daily coffee intake")
                             .font(.headline)
-
-                        Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                        
+                        Picker("", selection: bindingCoffeeAmount) {
+                            ForEach(1...6, id: \.self) {
+                                Text("\($0)")
+                            }
+                            
+                        }
+                        .pickerStyle(.segmented)
+                    }
+                }
+                
+                Section {
+                    VStack(alignment: .leading, spacing: 10) {
+                        Text(alertTitle)
+                            .font(.headline)
+                        
+                        Text(alertMessage)
+                            .font(.body)
+                            .pickerStyle(.segmented)
                     }
                 }
             }
             .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("OK") { }
-            } message: {
-                Text(alertMessage)
-            }
+        }
+        .onAppear {
+            calculateBedtime()
         }
     }
     
@@ -72,7 +106,7 @@ struct ContentView: View {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
-
+            
             let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
             let hour = (components.hour ?? 0) * 60 * 60
             let minute = (components.minute ?? 0) * 60
@@ -81,7 +115,7 @@ struct ContentView: View {
             
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "Your ideal bedtime is…"
+            alertTitle = "Your ideal bedtime is:"
             alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
             alertTitle = "Error"
