c8bca5ed9b1430fb80d9317fbd1ea60af597c30a
Serhii Hulenko <sergei.gulenko@gmail.com>
2022-08-01 18:05:09 +0200

    Storage, state, onDelete

---
+++ project_7/project_7/project_7App.swift
@@ -0,0 +1,17 @@
+//
+//  project_7App.swift
+//  project_7
+//
+//  Created by user on 01.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct project_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
